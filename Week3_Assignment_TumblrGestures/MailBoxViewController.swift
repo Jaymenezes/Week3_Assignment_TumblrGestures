e14d405e13dc879b156e8be783f893cee2634f13
JeanMenezes <jottamenezes@gmail.com>
2016-10-29 14:44:19 -0700

    Successfully panning right
    User can Pan the message to the right and have animation of background
    view and icons

--- Week3_Assignment_TumblrGestures/MailBoxViewController.swift
+++ Week3_Assignment_TumblrGestures/MailBoxViewController.swift
@@ -17,38 +17,38 @@ class MailBoxViewController: UIViewController, UIScrollViewDelegate, UIGestureRe
     @IBOutlet weak var messageImageView: UIImageView!
     
     @IBOutlet weak var backgroundView: UIView!
+
     var messageOriginalX: CGFloat!
     var laterImageViewOriginalX: CGFloat!
     var archiveImageViewOriginalX: CGFloat!
     var feedScrollViewOriginalX: CGFloat!
     
-    
-    
-    
+        
     @IBOutlet weak var feedScrollView: UIScrollView!
     @IBOutlet weak var feedImageView: UIImageView!
     
     
-    var messageOriginalCenter: CGPoint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        feedScrollView.contentSize = CGSize(width: 375, height: 1500)
+        feedScrollView.frame.size = CGSize(width: 375, height: 667)
+        
+        
         messageOriginalX = messageImageView.frame.origin.x
         laterImageViewOriginalX = laterImageView.frame.origin.x
         archiveImageViewOriginalX = archiveImageView.frame.origin.x
         feedScrollViewOriginalX = feedScrollView.frame.origin.x
         
-        feedScrollView.contentSize = CGSize(width: 375, height: 1500)
-        feedScrollView.frame.size = CGSize(width: 375, height: 667)
+
         
         
         let edgePanGestureRecognizer = UIScreenEdgePanGestureRecognizer(target: self, action: #selector(didPanFeedScrollView(sender:)))
-        
-        edgePanGestureRecognizer.edges = UIRectEdge.left
-        
         feedScrollView.isUserInteractionEnabled = true
         feedScrollView.addGestureRecognizer(edgePanGestureRecognizer)
         edgePanGestureRecognizer.delegate = self
+        edgePanGestureRecognizer.edges = UIRectEdge.left
+
         
         // Do any additional setup after loading the view.
     }
@@ -58,99 +58,127 @@ class MailBoxViewController: UIViewController, UIScrollViewDelegate, UIGestureRe
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func panMessage(_ sender: UIPanGestureRecognizer) {
-        let location = sender.location(in: view)
-        let translation = sender.translation(in: view)
-        print("l",location)
-        print(translation)
+    func didPanFeedScrollView (sender: UIScreenEdgePanGestureRecognizer) {
         
-        if sender.state == .began {
-            messageOriginalCenter = messageImageView.center
-            
-            
-            
-        } else if sender.state == .changed {
-            
-            
-            laterImageView.alpha = 0.2
+        let location = sender.location(in: view)
+        
+
+    }
+
+@IBAction func panMessage(_ sender: UIPanGestureRecognizer) {
+    let location = sender.location(in: view)
+    let translation = sender.translation(in: view)
+    print("l",location)
+    print(translation)
+    
+    if sender.state == .began {
+        
+        
+        
+    } else if sender.state == .changed {
+        
+        
+        laterImageView.alpha = 0.2
+        archiveImageView.alpha = 0.2
+        
+        laterImageView.image = UIImage(named: "later_icon")
+        archiveImageView.image = UIImage(named: "archive_icon")
+        
+        backgroundView.backgroundColor = UIColor.lightGray
+        messageImageView.frame.origin.x = messageOriginalX + translation.x
+        deleteImageView.alpha = 0
+
+
+        // panning messageImageView to the right
+
+        if messageImageView.frame.origin.x > 0 && messageImageView.frame.origin.x < 60 {
             archiveImageView.alpha = 0.2
-            
-            laterImageView.image = UIImage(named: "later_icon")
-            archiveImageView.image = UIImage(named: "archive_icon")
-            
             backgroundView.backgroundColor = UIColor.lightGray
+
+            archiveImageView.frame.origin.x = archiveImageViewOriginalX + (translation.x - 85)
+
+
+            
+        
+        } else if messageImageView.frame.origin.x > 60 && messageImageView.frame.origin.x < 260 {
+            
+            self.backgroundView.backgroundColor = UIColor.green
+            archiveImageView.alpha = 0.2
+            archiveImageView.frame.origin.x = archiveImageViewOriginalX + (translation.x - 85)
+
+            self.archiveImageView.alpha = 1
+
+
+            
+        } else if messageImageView.frame.origin.x > 260{
+            self.backgroundView.backgroundColor = UIColor.red
+            self.deleteImageView.alpha = 1
+            deleteImageView.frame.origin.x = archiveImageViewOriginalX + (translation.x - 85)
+
+            self.archiveImageView.alpha = 0
+            
+            UIView.animate(withDuration: 0.5, animations: {
+                
+            })
+
             
             
-            messageImageView.center = CGPoint(x: messageOriginalCenter.x + translation.x, y: messageOriginalCenter.y)
             
-            if messageImageView.frame.origin.x < 60 && messageImageView.frame.origin.x > -60 {
-                
-                UIView.animate(withDuration: 0.5, animations: {
-                    self.backgroundView.backgroundColor = UIColor.lightGray
-                })
-            } else if messageImageView.frame.origin.x < 0 && messageImageView.frame.origin.x > -260{
-                
-                archiveImageView.alpha = 0
-                
-                
-                // brown, list icon
-            } else if messageImageView.frame.origin.x < 0 && messageImageView.frame.origin.x > -375 {
-                
-                archiveImageView.alpha = 0
-                
-                
-            } else if messageImageView.frame.origin.x > 0 && messageImageView.frame.origin.x < 260 {
-                
-                UIView.animate(withDuration: 0.5, animations: {
-                    self.backgroundView.backgroundColor = UIColor.yellow
-                })
-                
-            } else if messageImageView.frame.origin.x > 0 && messageImageView.frame.origin.x < 260 {
-                
-                UIView.animate(withDuration: 0.5, animations: {
-                    self.backgroundView.backgroundColor = UIColor.green
-                })
-            } else if messageImageView.frame.origin.x > 0 && messageImageView.frame.origin.x < 375 {
-                
-                UIView.animate(withDuration: 0.5, animations: {
-                    self.backgroundView.backgroundColor = UIColor.red
-                })
-                
-            }
-        } else if sender.state == .ended {
+        } else if messageImageView.frame.origin.x < 0 && messageImageView.frame.origin.x > -375 {
+            
+            
+            
+        
+        } else if messageImageView.frame.origin.x > 0 && messageImageView.frame.origin.x < 260 {
+            
+            UIView.animate(withDuration: 0.5, animations: {
+                self.backgroundView.backgroundColor = UIColor.green
+            })
+        } else if messageImageView.frame.origin.x > 0 && messageImageView.frame.origin.x < 375 {
+            
+            UIView.animate(withDuration: 0.5, animations: {
+                self.backgroundView.backgroundColor = UIColor.red
+            })
             
         }
-        
-        //                                        archiveImageView.image = UIImage(named: "archive_icon")
-        //                                        archiveImageView.frame.origin.x = archiveImageViewOriginalX + (translation.x - 60)
-        
-        
-        //            } else messageImageView.frame.origin.x > 0 && messageImageView.frame.origin.x < 375 {
-        //
-        //                UIView.animate(withDuration: 0.3, animations: {
-        //
-        //                    }//
+    } else if sender.state == .ended {
+        messageImageView.frame.origin.x = messageOriginalX + translation.x
+
+
         
         
     }
     
+    //                                        archiveImageView.image = UIImage(named: "archive_icon")
+    //                                        archiveImageView.frame.origin.x = archiveImageViewOriginalX + (translation.x - 60)
     
-    //            if location.y > 395 {
-    //                UIView.animate(withDuration: 0.4, delay: 0.1, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.7, options: [], animations: {
+    
+    //            } else messageImageView.frame.origin.x > 0 && messageImageView.frame.origin.x < 375 {
     //
-    //                    let imageView = sender.view as! UIImageView
+    //                UIView.animate(withDuration: 0.3, animations: {
     //
-    //                    self.newlyCreatedFace.center = imageView.center
-    //                    self.newlyCreatedFace.center.y += self.trayView.frame.origin.y
-    //                    self.newlyCreatedFace.center.x += self.trayView.frame.origin.x
-    //                    self.newlyCreatedFace.transform = self.newlyCreatedFace.transform.scaledBy(x: 0.8, y: 0.8)
-    //                }) { (Bool) in
-    //                    self.newlyCreatedFace.removeFromSuperview()
-    //
-    //                }
+    //                    }//
     
     
-    
+}
+
+
+//            if location.y > 395 {
+//                UIView.animate(withDuration: 0.4, delay: 0.1, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.7, options: [], animations: {
+//
+//                    let imageView = sender.view as! UIImageView
+//
+//                    self.newlyCreatedFace.center = imageView.center
+//                    self.newlyCreatedFace.center.y += self.trayView.frame.origin.y
+//                    self.newlyCreatedFace.center.x += self.trayView.frame.origin.x
+//                    self.newlyCreatedFace.transform = self.newlyCreatedFace.transform.scaledBy(x: 0.8, y: 0.8)
+//                }) { (Bool) in
+//                    self.newlyCreatedFace.removeFromSuperview()
+//
+//                }
+
+
+
 }
 
 
